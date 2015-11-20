b7829d26121dae0ea07b87f7c2b86ee3e218c02b
Tim <tim199178@yandex.com>
2015-11-20 15:21:38 +0200

    historique page

---
+++ dist/assets/js/historique.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  var $window = $(window);
+  var $body = $('body');
+
+  var $container = $('#historiquebody');
+
+  $container.isotope({
+    itemSelector: '.entry',
+    masonry: {
+      columnWidth: '.entry'
+    }
+  });
+
+
+  function timelineEntries(){
+    $('#historiquebody').find('.entry').each( function(){
+      var position = $(this).inlineStyle('left');
+      if( position == '0px' ) {
+        $(this).removeClass('alt');
+      } else {
+        $(this).addClass('alt');
+      }
+      $(this).find('.entry-timeline').fadeIn();
+    });
+  }
+
+  setTimeout( function(){
+    timelineEntries();
+  }, 2500 );
+
+  $(window).resize(function() {
+    $container.isotope('layout');
+    var t = setTimeout( function(){
+      timelineEntries();
+    }, 2500 );
+  });
+
+});
