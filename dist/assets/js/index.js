07f9eb010f3be7858c799e9491da0c3599e636ab
Tim <tim199178@yandex.com>
2015-12-02 12:29:25 +0200

    integrate logocarousel

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
+window.jQuery = window.$ = jQuery
+
+$(document).ready(function () {
   var $window = $(window);
   var $body = $('body');
   var $navbar = $("#mainNav");
   var $hero = $('#hero.full-screen');
 
   // Hero Parallax
-  var heroParallaxOffset = function(){
+  var heroParallaxOffset = function () {
     var sliderParallaxOffsetTop = 0;
     var headerHeight = $navbar.outerHeight();
     sliderParallaxOffsetTop = headerHeight;
@@ -15,40 +17,39 @@ $(document).ready(function(){
     return sliderParallaxOffsetTop;
   }
 
-  var heroSliderParallax = function(){
+  var heroSliderParallax = function () {
     // if( ( $body.hasClass('device-lg') || $body.hasClass('device-md') ) && !SEMICOLON.isMobile.any() ) {
-      var parallaxOffsetTop = heroParallaxOffset();
-      var parallaxElHeight = $hero.outerHeight();
+    var parallaxOffsetTop = heroParallaxOffset();
+    var parallaxElHeight = $hero.outerHeight();
 
-      if( ( parallaxElHeight + parallaxOffsetTop + 50 ) > $window.scrollTop() ){
-        if ($window.scrollTop() > parallaxOffsetTop) {
-          var tranformAmount = (($window.scrollTop()-parallaxOffsetTop) / 1.5 ).toFixed(2);
-          var tranformAmount2 = (($window.scrollTop()-parallaxOffsetTop) / 7 ).toFixed(2);
-          $hero.stop(true,true).transition({ y: tranformAmount },0);
-        }
+    if (( parallaxElHeight + parallaxOffsetTop + 50 ) > $window.scrollTop()) {
+      if ($window.scrollTop() > parallaxOffsetTop) {
+        var tranformAmount = (($window.scrollTop() - parallaxOffsetTop) / 1.5 ).toFixed(2);
+        var tranformAmount2 = (($window.scrollTop() - parallaxOffsetTop) / 7 ).toFixed(2);
+        $hero.stop(true, true).transition({y: tranformAmount}, 0);
       }
-      // if (requesting) {
-      //   requestAnimationFrame(function(){
-      //     heroSliderParallax();
-      //     // SEMICOLON.slider.sliderElementsFade();
-      //   });
-      // }
-      // } else {
-      //   $('.slider-parallax,.slider-parallax .slider-caption,.ei-title').transition({ y: 0 },0);
-      // }
+    }
+    // if (requesting) {
+    //   requestAnimationFrame(function(){
+    //     heroSliderParallax();
+    //     // SEMICOLON.slider.sliderElementsFade();
+    //   });
+    // }
+    // } else {
+    //   $('.slider-parallax,.slider-parallax .slider-caption,.ei-title').transition({ y: 0 },0);
+    // }
   }
 
   // heroSliderParallax();
 
 
-
   // Hero vertical align middle
-  var heroVerticalAlign = function(){
+  var heroVerticalAlign = function () {
     var $heroOver = $hero.find('.slider-over');
     $heroOver.css({
       position: 'absolute',
       top: '42%',
-      marginTop: - ($heroOver.outerHeight()/2)+'px'
+      marginTop: -($heroOver.outerHeight() / 2) + 'px'
     });
   }
 
@@ -57,16 +58,16 @@ $(document).ready(function(){
 
 
   // Hero Fullscreen
-  var heroFullscreen = function(){
+  var heroFullscreen = function () {
     var scrHeight = $window.height();
     var sliderHeightOff = $hero.offset().top;
     scrHeight = scrHeight - sliderHeightOff;
-    if($hero.hasClass('parallax')) {
+    if ($hero.hasClass('parallax')) {
       var transformVal = $hero.css('transform');
       var transformX = transformVal.match(/-?[\d\.]+/g);
       var transformXvalue = (transformX) ? transformX[5] : 0;
 
-      scrHeight = ( $window.height() + Number( transformXvalue ) ) - sliderHeightOff;
+      scrHeight = ( $window.height() + Number(transformXvalue) ) - sliderHeightOff;
     }
     $hero.css('height', scrHeight);
   }
@@ -76,18 +77,18 @@ $(document).ready(function(){
 
 
   // Hero Swiper
-  var swiperSlider = new Swiper('#hero .swiper-container',{
+  var swiperSlider = new Swiper('#hero .swiper-container', {
     paginationClickable: false,
     slidesPerView: 1,
     grabCursor: true,
   });
 
-  $('#hero .swiper-button-prev').on('click', function(e){
+  $('#hero .swiper-button-prev').on('click', function (e) {
     e.preventDefault();
     swiperSlider.slidePrev();
   });
 
-  $('#hero .swiper-button-next').on('click', function(e){
+  $('#hero .swiper-button-next').on('click', function (e) {
     e.preventDefault();
     swiperSlider.slideNext();
   });
@@ -96,17 +97,57 @@ $(document).ready(function(){
   $('#homeInstagram .owl-carousel').owlCarousel({
     margin: 20,
     nav: true,
-    navText: ['<i class="icon-angle-left"></i>','<i class="icon-angle-right"></i>'],
+    navText: ['<i class="icon-angle-left"></i>', '<i class="icon-angle-right"></i>'],
     autoplay: false,
     autoplayHoverPause: true,
     dots: false,
     navRewind: false,
-    responsive:{
-      0:{ items:2 },
-      600:{ items:3 },
-      1000:{ items:4 },
-      1200:{ items:5 }
+    responsive: {
+      0: {items: 2},
+      600: {items: 3},
+      1000: {items: 4},
+      1200: {items: 5}
     }
+  });
+
+
+  function initlalizlogoCarousel(){
+    var width = $(window).width();
+    var cols = 12;
+    if(width>=1200){
+      cols = 12;
+    }else if(width>992){
+      cols = 10;
+    }else if(width>767){
+      cols = 6;
+    }else{
+      cols = 4;
+    }
+
+    // Use default configuration
+    $('#logo_carousel').logoCarousel({
+      cols: cols,
+      alignment: 'vertical',
+      verticalDir: 'up',
+      speed: 350,
+      height: 480,
+      tbPadding: 20,
+      lrPadding: 15,
+      hoverStop: true,
+      forceFullwidth: true,
+      bgColor: '#d29389',
+      logoOpacity: 0.6,
+      hoverOpacity: 1
+    });
+  }
+
+  //logo carousel
+  $('body').waitForImages(function () {
+    initlalizlogoCarousel();
+  });
+
+  $(window).on("debouncedresize", function(e) {
+    //initlalizlogoCarousel()
   });
 
 });
