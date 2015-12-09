aad9de5b67e94280938779033f58e06b2cfe98e3
Francis Poirier <fpoirier@gmail.com>
2015-12-09 18:54:06 -0500

    Section nouvelles

---
+++ dist/assets/js/nouvelles.js
@@ -0,0 +1,39 @@
+(function($) {
+
+  
+  $(document).ready(function(){
+    if($("#instagramNewsFeed").length > 0){
+      // Instagram Feed  
+      var feed = new Instafeed({
+        accessToken: '1706625845.7c96a79.5cded748c4cd4f72a7d9b5deb3a74e55',
+        userId: '1706625845',
+        // accessToken: '2215496639.d52792e.1c38bdfc523e45668120e6a444c7664e',
+        // userId: '2215496639',
+        target: 'instagramNewsFeed',
+        get: 'user',
+        resolution: 'low_resolution',
+        limit : 15,
+        filter: function(image) {
+          var imageDate = new Date(parseInt(image.created_time)*1000);
+          var timeAgo = moment(imageDate).locale('fr').fromNow();
+          console.log(timeAgo);
+          image.created_time_ago = timeAgo;
+
+          return true;
+        },
+        template: '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">\
+                    <div class="news-item thumbnail">\
+                      <a href="{{link}}" class="news-object"><img src="{{image}}" /></a>\
+                      <div class="news-body">\
+                        <a href="{{link}}">@chartaconstruction</a> <span class="news-time"> - {{model.created_time_ago}}</span>\
+                        <p>{{caption}}</p>\
+                      </div>\
+                    </div>\
+                  </div>',      
+      });
+      feed.run();
+    }
+  })
+  
+
+})(jQuery)
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,128 +6,133 @@
     var $navbar = $("#mainNav");
     var $hero = $('#hero.full-screen');
 
-    // Hero Parallax
-    var heroParallaxOffset = function () {
-      var sliderParallaxOffsetTop = 0;
-      var headerHeight = $navbar.outerHeight();
-      sliderParallaxOffsetTop = headerHeight;
 
-      // if( $hero.next('#header').length > 0 ) { sliderParallaxOffsetTop = 0; }
+    // Hero Parallax    
+    if($hero.length > 0){
 
-      return sliderParallaxOffsetTop;
-    }
+      var heroParallaxOffset = function () {
+        var sliderParallaxOffsetTop = 0;
+        var headerHeight = $navbar.outerHeight();
+        sliderParallaxOffsetTop = headerHeight;
 
-    var heroSliderParallax = function () {
-      // if( ( $body.hasClass('device-lg') || $body.hasClass('device-md') ) && !SEMICOLON.isMobile.any() ) {
-      var parallaxOffsetTop = heroParallaxOffset();
-      var parallaxElHeight = $hero.outerHeight();
+        // if( $hero.next('#header').length > 0 ) { sliderParallaxOffsetTop = 0; }
 
-      if (( parallaxElHeight + parallaxOffsetTop + 50 ) > $window.scrollTop()) {
-        if ($window.scrollTop() > parallaxOffsetTop) {
-          var tranformAmount = (($window.scrollTop() - parallaxOffsetTop) / 1.5 ).toFixed(2);
-          var tranformAmount2 = (($window.scrollTop() - parallaxOffsetTop) / 7 ).toFixed(2);
-          $hero.stop(true, true).transition({y: tranformAmount}, 0);
-        }
+        return sliderParallaxOffsetTop;
       }
-      // if (requesting) {
-      //   requestAnimationFrame(function(){
-      //     heroSliderParallax();
-      //     // SEMICOLON.slider.sliderElementsFade();
-      //   });
-      // }
-      // } else {
-      //   $('.slider-parallax,.slider-parallax .slider-caption,.ei-title').transition({ y: 0 },0);
-      // }
-    }
 
-    // heroSliderParallax();
+      var heroSliderParallax = function () {
+        // if( ( $body.hasClass('device-lg') || $body.hasClass('device-md') ) && !SEMICOLON.isMobile.any() ) {
+        var parallaxOffsetTop = heroParallaxOffset();
+        var parallaxElHeight = $hero.outerHeight();
+
+        if (( parallaxElHeight + parallaxOffsetTop + 50 ) > $window.scrollTop()) {
+          if ($window.scrollTop() > parallaxOffsetTop) {
+            var tranformAmount = (($window.scrollTop() - parallaxOffsetTop) / 1.5 ).toFixed(2);
+            var tranformAmount2 = (($window.scrollTop() - parallaxOffsetTop) / 7 ).toFixed(2);
+            $hero.stop(true, true).transition({y: tranformAmount}, 0);
+          }
+        }
+        // if (requesting) {
+        //   requestAnimationFrame(function(){
+        //     heroSliderParallax();
+        //     // SEMICOLON.slider.sliderElementsFade();
+        //   });
+        // }
+        // } else {
+        //   $('.slider-parallax,.slider-parallax .slider-caption,.ei-title').transition({ y: 0 },0);
+        // }
+      }
+
+      // heroSliderParallax();
 
 
-    // Hero vertical align middle
-    var heroVerticalAlign = function () {
-      var $heroOver = $hero.find('.slider-over');
-      $heroOver.css({
-        position: 'absolute',
-        top: '42%',
-        marginTop: -($heroOver.outerHeight() / 2) + 'px'
+      // Hero vertical align middle
+      var heroVerticalAlign = function () {
+        var $heroOver = $hero.find('.slider-over');
+        $heroOver.css({
+          position: 'absolute',
+          top: '42%',
+          marginTop: -($heroOver.outerHeight() / 2) + 'px'
+        });
+      }
+
+      heroVerticalAlign();
+      $window.on('resize', heroVerticalAlign);
+
+
+      // Hero Fullscreen
+      var heroFullscreen = function () {
+        var scrHeight = $window.height();
+        var sliderHeightOff = $hero.offset().top;
+        scrHeight = scrHeight - sliderHeightOff;
+        if ($hero.hasClass('parallax')) {
+          var transformVal = $hero.css('transform');
+          var transformX = transformVal.match(/-?[\d\.]+/g);
+          var transformXvalue = (transformX) ? transformX[5] : 0;
+
+          scrHeight = ( $window.height() + Number(transformXvalue) ) - sliderHeightOff;
+        }
+        $hero.css('height', scrHeight);
+      }
+
+      heroFullscreen();
+      $window.on('resize', heroFullscreen);
+
+
+      // Hero Swiper
+      var swiperSlider = new Swiper('#hero .swiper-container', {
+        paginationClickable: false,
+        slidesPerView: 1,
+        grabCursor: true,
+      });
+
+      $('#hero .swiper-button-prev').on('click', function (e) {
+        e.preventDefault();
+        swiperSlider.slidePrev();
+      });
+
+      $('#hero .swiper-button-next').on('click', function (e) {
+        e.preventDefault();
+        swiperSlider.slideNext();
       });
     }
 
-    heroVerticalAlign();
-    $window.on('resize', heroVerticalAlign);
-
-
-    // Hero Fullscreen
-    var heroFullscreen = function () {
-      var scrHeight = $window.height();
-      var sliderHeightOff = $hero.offset().top;
-      scrHeight = scrHeight - sliderHeightOff;
-      if ($hero.hasClass('parallax')) {
-        var transformVal = $hero.css('transform');
-        var transformX = transformVal.match(/-?[\d\.]+/g);
-        var transformXvalue = (transformX) ? transformX[5] : 0;
-
-        scrHeight = ( $window.height() + Number(transformXvalue) ) - sliderHeightOff;
-      }
-      $hero.css('height', scrHeight);
-    }
-
-    heroFullscreen();
-    $window.on('resize', heroFullscreen);
-
-
-    // Hero Swiper
-    var swiperSlider = new Swiper('#hero .swiper-container', {
-      paginationClickable: false,
-      slidesPerView: 1,
-      grabCursor: true,
-    });
-
-    $('#hero .swiper-button-prev').on('click', function (e) {
-      e.preventDefault();
-      swiperSlider.slidePrev();
-    });
-
-    $('#hero .swiper-button-next').on('click', function (e) {
-      e.preventDefault();
-      swiperSlider.slideNext();
-    });
-
-
 
     // Instagram Carousel  
-
-    var feed = new Instafeed({
-      accessToken: '2215496639.d52792e.1c38bdfc523e45668120e6a444c7664e',
-      target: 'instagramFeed',
-      get: 'user',
-      userId: '2215496639',
-      resolution: 'low_resolution',
-      limit : 15,
-      template: '<div><a href="{{link}}"><img src="{{image}}" /></a></div>',
-      success : function(){
-        setTimeout(function(){
-          $('#homeInstagram .owl-carousel').owlCarousel({
-            margin: 20,
-            nav: true,
-            navText: ['<i class="icon-angle-left"></i>', '<i class="icon-angle-right"></i>'],
-            autoplay: false,
-            autoplayHoverPause: true,
-            dots: false,
-            navRewind: false,
-            responsive: {
-              0: {items: 3},
-              600: {items: 4},
-              1000: {items: 5},
-              1200: {items: 6}
-            }
-          });
-        })
-        
-      }
-    });
-    feed.run();
-
+    if($("#instagramHomeFeed").length > 0){
+      var feed = new Instafeed({
+        accessToken: '1706625845.7c96a79.5cded748c4cd4f72a7d9b5deb3a74e55',
+        userId: '1706625845',
+        // accessToken: '2215496639.d52792e.1c38bdfc523e45668120e6a444c7664e',
+        // userId: '2215496639',
+        target: 'instagramHomeFeed',
+        get: 'user',
+        resolution: 'low_resolution',
+        limit : 15,
+        template: '<div><a href="{{link}}"><img src="{{image}}" /></a></div>',
+        success : function(){
+          setTimeout(function(){
+            $('#homeInstagram .owl-carousel').owlCarousel({
+              margin: 20,
+              nav: true,
+              navText: ['<i class="icon-angle-left"></i>', '<i class="icon-angle-right"></i>'],
+              autoplay: false,
+              autoplayHoverPause: true,
+              dots: false,
+              navRewind: false,
+              responsive: {
+                0: {items: 3},
+                600: {items: 4},
+                1000: {items: 5},
+                1200: {items: 6}
+              }
+            });
+          })
+          
+        }
+      });
+      feed.run();
+    }
 
 
     // LOGO CAROUSEL 
