f2e02e109b60930b3349a4de490adede8ad102fd
Francis Poirier <fpoirier@gmail.com>
2015-12-10 16:37:17 -0500

    Fix historique js

--- dist/assets/js/historique.js
+++ dist/assets/js/historique.js
@@ -1,40 +1,42 @@
-$(document).ready(function () {
-  var $window = $(window);
-  var $body = $('body');
+(function($){
+  $(document).ready(function () {
+    var $window = $(window);
+    var $body = $('body');
 
-  var $container = $('#historiquebody');
+    var $container = $('#historiquebody');
 
-  $container.isotope({
-    itemSelector: '.entry',
-    masonry: {
-      columnWidth: '.entry'
-    }
-  });
-
-
-  function timelineEntries(){
-    $('#historiquebody').find('.entry').each( function(){
-      var position = $(this).inlineStyle('left');
-      if( position == '0px' ) {
-        $(this).addClass('art');
-        $(this).removeClass('alt');
-      } else {
-        $(this).removeClass('art');
-        $(this).addClass('alt');
+    $container.isotope({
+      itemSelector: '.entry',
+      masonry: {
+        columnWidth: '.entry'
       }
-      $(this).find('.entry-timeline').fadeIn();
     });
-  }
 
-  setTimeout( function(){
-    timelineEntries();
-  }, 500 );
 
-  $(window).resize(function() {
-    $container.isotope('layout');
-    var t = setTimeout( function(){
+    function timelineEntries(){
+      $('#historiquebody').find('.entry').each( function(){
+        var position = $(this).inlineStyle('left');
+        if( position == '0px' ) {
+          $(this).addClass('art');
+          $(this).removeClass('alt');
+        } else {
+          $(this).removeClass('art');
+          $(this).addClass('alt');
+        }
+        $(this).find('.entry-timeline').fadeIn();
+      });
+    }
+
+    setTimeout( function(){
       timelineEntries();
     }, 500 );
-  });
 
-});
+    $(window).resize(function() {
+      $container.isotope('layout');
+      var t = setTimeout( function(){
+        timelineEntries();
+      }, 500 );
+    });
+
+  });
+})(jQuery)
