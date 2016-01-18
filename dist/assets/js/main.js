9ca348fdf3372df1ccf813812fb13ec1ee974f3b
Francis Poirier <fpoirier@gmail.com>
2016-01-18 12:52:45 -0500

    no message

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,14 +1,40 @@
-var endpointUrl = "http://secondhandboards.com/sdw/sentmail/sendmail.php";
 (function($) {
   $(document).ready(function(){
     var $window = $(window);
     var $body = $('body');
     var $navbar = $("#mainNav");
 
+    jRes = jRespond([
+        {
+          label: 'phone',
+          enter: 0,
+          exit: 767
+        },{
+          label: 'tablet',
+          enter: 768,
+          exit: 991
+        },{
+          label: 'laptop',
+          enter: 992,
+          exit: 1199
+        },{
+          label: 'desktop',
+          enter: 1200,
+          exit: 10000
+        }
+    ]);
+
     // Navigation
     $("#mainNav .nav").superfish({
       pathClass : 'current'
     });
+    // Prevent the click on parent item in mobile mode
+    $("#mainNav .nav a.sf-with-ul").click(function(e){
+      var breakpoint = jRes.getBreakpoint();
+
+      if(breakpoint == "phone" || breakpoint == "tablet")
+        e.preventDefault();
+    })
 
     $('.fancybox').fancybox();
 
